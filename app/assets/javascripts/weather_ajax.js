b6ae86b3ad4be79eadd9fa7c471d164382fdae07
ralph kemp <ralphk3mp@gmail.com>
2017-12-30 22:58:30 +0000

    -

--- app/assets/javascripts/weather_ajax.js
+++ app/assets/javascripts/weather_ajax.js
@@ -1,20 +1,18 @@
 const button = document.getElementById('button');
 const search = document.getElementById('search');
 const weatherContainer = document.getElementById('weather-container');
+const results = document.getElementById('results');
+
+
 
 button.addEventListener("click", (event) => {
-  fetch(`http://api.openweathermap.org/data/2.5/forecast?id=${search.value}&appid=231e634ee102fa27f134aef8711b9a05`)
+  fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${search.value},UK&appid=231e634ee102fa27f134aef8711b9a05`)
     .then(response => response.json())
     .then((data) => {
-      const name = data.city.name;
+      const id = data.city.id;
       const country = data.city.country;
-      const all = `<h3> the name is ${name} and the country is ${country} </h3>`;
+      const all = `<h3> the city ID is ${id} and the country is ${country} </h3>`;
 
       weatherContainer.insertAdjacentHTML('afterbegin', all);
     });
 });
-
-
-
-
-// london = 2643743
