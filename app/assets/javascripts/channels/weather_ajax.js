95f46cd2d8d5d9fe5f219df35f8893791fa972b2
ralph kemp <ralphk3mp@gmail.com>
2017-12-30 21:40:39 +0000

    still working on the best way to display basic city data

--- app/assets/javascripts/channels/weather_ajax.js
+++ app/assets/javascripts/channels/weather_ajax.js
@@ -1,16 +1,19 @@
 const button = document.getElementById('button');
 const search = document.getElementById('search');
+const weatherContainer = document.getElementById('weather-container');
 
 button.addEventListener("click", (event) => {
   fetch(`http://api.openweathermap.org/data/2.5/forecast?id=${search.value}&appid=231e634ee102fa27f134aef8711b9a05`)
     .then(response => response.json())
     .then((data) => {
       const name = data.city.name;
-      console.log(name);
+      const country = data.city.country;
+      const all = `the name is ${name} and the country is ${country}`;
+      weatherContainer.insertAdjacentHTML('afterbegin', all);
     });
 });
 
 
 
 
-// 2915013
+// london = 2643743
