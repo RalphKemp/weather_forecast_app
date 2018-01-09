ff986097f7012d650f201b715d5b36c78a5addd2
ralph kemp <ralphk3mp@gmail.com>
2018-01-09 00:59:56 +0000

    -

--- app/javascript/weather_json.js
+++ app/javascript/weather_json.js
@@ -114,14 +114,6 @@ formValidation();
         console.log(median(two));
 
 
-
-
-
-
-
-
-
-
         function dayName(date) {
           const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
           const day = new Date(date);
